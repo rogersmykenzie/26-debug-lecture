1ae36d122ac8398caf4cafe66e6fa07910325605
Mykenzie Rogers <mykenzierogers@gmail.com>
2020-01-28 09:11:37 -0600

    touchup

--- controller.js
+++ controller.js
@@ -1,32 +1,31 @@
 function makeEvensArray() {
-    let myEvensArr = [];
-    while(myEvensArr.length <= 10) {
-        let myNum = getRandomNumber();
-        if(checkIfEven(myNum)) {
-            myEvensArr.push(myNum);
-            return myEvensArr;
-        }
+  let myEvensArr = [];
+  while (myEvensArr.length <= 10) {
+    let myNum = getRandomNumber();
+    if (checkIfEven(myNum)) {
+      myEvensArr.push(myNum);
+      return myEvensArr;
     }
+  }
 }
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 100);
 }
 
 function checkIfEven(num) {
-    if(num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getEvensArray(req, res) {
-    debugger;
-    let myArr = makeEvensArray();
-    res.status(200).json(myArr);
-} 
+  let myArr = makeEvensArray();
+  res.status(200).json(myArr);
+}
 
 module.exports = {
-    getEvensArray
-}
+  getEvensArray
+};
